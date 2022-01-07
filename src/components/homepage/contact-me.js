b600337da86fb7ad0fdb17b1e2917a396d67a9e0
Olie Larner <olie@silver.agency>
2022-01-07 23:33:20 +0000

    sdsds

---
+++ src/components/homepage/contact-me.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react"
+
+const ContactMe = () => {
+  return (
+    <div className="w-full py-32">
+      <div className="w-10/12 mx-auto flex">
+        <h3>That's about it...</h3>
+        <p>If you want to get in contact</p>
+      </div>
+    </div>
+  )
+}
